5bbb259ac4d69a0851df905f947b65a6af5456e8
phossein <phossein@redhat.com>
2018-11-20 15:21:38 -0500

    new function that extracts metadata from DataFrame

--- stucco.py
+++ stucco.py
@@ -1,3 +1,7 @@
+import json
+import numpy as np
+import pandas as pd
+
 from itertools import combinations
 
 
@@ -24,3 +28,37 @@ def canonical_combination(items, max_length=None):
             yield subset
         if length == max_length:
             break
+
+
+def get_frame_metadata(frame, num_splits=3, max_unique_reals=15, out=None):
+    """
+    Derive the metadata for a pandas DataFrame. Metadata is defined as the
+    data-type and data-range of a feature.
+    """
+
+    # ensure the number of bins to split floats into is a valid integer
+    if num_splits is None or num_splits < 1:
+        raise ValueError('`num_splits` must be a positive integer.')
+
+    # store metadata given a DataFrame
+    metadata = {}
+
+    # handle categorical data-points, i.e. category, boolean, and strings
+    subset = frame.select_dtypes(['category', 'bool', 'object'])
+    for col_name in subset:
+        values = subset[col_name].unique()
+        record = {'items': values.tolist(), 'data_type': str(values.dtype)}
+        metadata[col_name] = record
+
+    # handle real-data; too many points makes them continuous in-range
+    subset = frame.select_dtypes(['int', 'float'])
+    for col_name in subset:
+        values = subset[col_name].unique()
+        if len(values) > max_unique_reals:
+            splits = np.array_split(values, num_splits)
+            values = np.asarray(list(map(lambda x: list(x[[0, -1]]), splits)))
+        record = {'items': values.tolist(), 'data_type': str(values.dtype)}
+        metadata[col_name] = record
+
+    if out:
+        json.dump(metadata, open(out, 'w'), indent=4)
